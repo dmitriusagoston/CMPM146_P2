a6fcc997043acbcd4f0e0e0b0a2aeb31faf42665
dmitriusagoston <dmitri.agoston@gmail.com>
2024-01-22 18:51:08 -0800

    minor experiment change

--- src/experiment1.py
+++ src/experiment1.py
@@ -1,17 +1,36 @@
 import subprocess
 import time
-script_path = '.\src\p2_exp.py'
+script_path = '.\\src\\p2_exp.py'
+arguments50 = ['mcts_vanilla', 'exp_mcts_vanilla', '50', '2.']
 arguments200 = ['mcts_vanilla', 'exp_mcts_vanilla', '200', '2.']
 arguments500 = ['mcts_vanilla', 'exp_mcts_vanilla', '500', '2.']
 arguments1000 = ['mcts_vanilla', 'exp_mcts_vanilla', '1000', '2.']
-arguments1500 = ['mcts_vanilla', 'exp_mcts_vanilla', '1500', '2.']
+
 
 # Construct the command to run the script
+command50 = ['python', script_path] + arguments50
 command200 = ['python', script_path] + arguments200
 command500 = ['python', script_path] + arguments500
 command1000 = ['python', script_path] + arguments1000
-command1500 = ['python', script_path] + arguments1500
 
+print("Starting 50 nodes test \n")
+print("Processing: ", end='', flush=True)
+for _ in range(2):
+    print("\rProcessing: {}%".format(_), end='', flush=True)
+    # Run the script using subprocess and capture the output
+    try:
+        result = subprocess.run(command50, check=True, capture_output=True, text=True)
+        
+        # Save the output to a file
+        with open('output50.csv', 'a', newline='') as output_file:
+            output_file.write(result.stdout)
+            
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        # Optionally, you can also save the error output to a file
+        with open('error_output.txt', 'w') as error_output_file:
+            error_output_file.write(e.stderr)
+print("Finished 50 nodes test \n")
 print("Starting 200 nodes test \n")
 print("Processing: ", end='', flush=True)
 for _ in range(2):
@@ -66,21 +85,3 @@ for _ in range(2):
         with open('error_output.txt', 'w') as error_output_file:
             error_output_file.write(e.stderr)
 print("Finished 1000 nodes test \n")
-print("Starting 1500 nodes test \n")
-print("Processing: ", end='', flush=True)
-for _ in range(2):
-    print("\rProcessing: {}%".format(_), end='', flush=True)
-    # Run the script using subprocess and capture the output
-    try:
-        result = subprocess.run(command1500, check=True, capture_output=True, text=True)
-        
-        # Save the output to a file
-        with open('output1500.csv', 'a', newline='') as output_file:
-            output_file.write(result.stdout)
-            
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
-        # Optionally, you can also save the error output to a file
-        with open('error_output.txt', 'w') as error_output_file:
-            error_output_file.write(e.stderr)
-print("Finished 1500 nodes test \n")
